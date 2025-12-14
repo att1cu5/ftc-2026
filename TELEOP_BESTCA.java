275c3f8aefceab43c68a51889bb769fe89761ad4
att1cu5 <atticusmgr27@rdcrs.ca>
2025-12-13 22:40:00 -0700

    Enable webcam and initialize AprilTag processing

--- TELEOP_BESTCA.java
+++ TELEOP_BESTCA.java
@@ -48,11 +48,11 @@ public class TELEOP_BESTC extends LinearOpMode {
   double runtime=0;
   double starty=0;
   double deltaY=0;
-  //boolean USE_WEBCAM;
-  //AprilTagProcessor myAprilTagProcessor;
-  //Position cameraPosition;
-  //YawPitchRollAngles cameraOrientation;
-  //VisionPortal myVisionPortal;
+  boolean USE_WEBCAM;
+  AprilTagProcessor myAprilTagProcessor;
+  Position cameraPosition;
+  YawPitchRollAngles cameraOrientation;
+  VisionPortal myVisionPortal;
   double rangeB=0; 
   public double offsetX=9.129397076417323;
   public double offsetY=5.1496063;//fine tune this
@@ -199,6 +199,7 @@ public class TELEOP_BESTC extends LinearOpMode {
   }
   @Override
   public void runOpMode() {
+    USE_WEBCAM = true;
     //private ElapsedTime runtime = new ElapsedTime();
     pivotintake = hardwareMap.get(Servo.class, "pivotintake");
     pivotintakeA = hardwareMap.get(Servo.class, "pivotintakeA");
@@ -241,9 +242,16 @@ public class TELEOP_BESTC extends LinearOpMode {
     intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     X.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+
+    cameraPosition = new Position(DistanceUnit.INCH, 0, 0, 0, 0);
+    cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES, 0, -90, 0, 0);
+    // Initialize AprilTag before waitForStart.
+    initAprilTag();
+    // Wait for the match to begin.
     telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
     telemetry.addData(">", "Touch START to start OpMode");
     telemetry.update();
+
     waitForStart();
     //runtime.reset();
     while (opModeIsActive()) {
@@ -294,6 +302,10 @@ public class TELEOP_BESTC extends LinearOpMode {
            
       }
       if(gamepad1.right_bumper){
+           intake.setPower(-speedOfintakeOn); 
+           
+      }
+      else{
         
           intake.setPower(speedOfintakeOff); 
           
@@ -380,10 +392,95 @@ public class TELEOP_BESTC extends LinearOpMode {
               backleft.setPower(swerve_B);            
               backright.setPower(swerve_B);
       }
-      
+
+
+      telemetryAprilTag();
+      // Push telemetry to the Driver Station.
+      telemetry.update();
+      if (gamepad1.dpad_down) {
+        // Temporarily stop the streaming session. This can save CPU
+        // resources, with the ability to resume quickly when needed.
+        myVisionPortal.stopStreaming();
+      } else if (gamepad1.dpad_up) {
+        // Resume the streaming session if previously stopped.
+        myVisionPortal.resumeStreaming();
+      }
+      // Share the CPU.
+      sleep(20);
     }
   }
+
+  /**
+   * Initialize AprilTag Detection.
+   */
+  private void initAprilTag() {
+    AprilTagProcessor.Builder myAprilTagProcessorBuilder;
+    VisionPortal.Builder myVisionPortalBuilder;
+
+    // First, create an AprilTagProcessor.Builder.
+    myAprilTagProcessorBuilder = new AprilTagProcessor.Builder();
+    myAprilTagProcessorBuilder.setCameraPose(cameraPosition, cameraOrientation);
+    // Create an AprilTagProcessor by calling build.
+    myAprilTagProcessor = myAprilTagProcessorBuilder.build();
+    // Next, create a VisionPortal.Builder and set attributes related to the camera.
+    myVisionPortalBuilder = new VisionPortal.Builder();
+    if (USE_WEBCAM) {
+      // Use a webcam.
+      myVisionPortalBuilder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+    } else {
+      // Use the device's back camera.
+      myVisionPortalBuilder.setCamera(BuiltinCameraDirection.BACK);
+    }
+    // Add myAprilTagProcessor to the VisionPortal.Builder.
+    myVisionPortalBuilder.addProcessor(myAprilTagProcessor);
+    // Create a VisionPortal by calling build.
+    myVisionPortal = myVisionPortalBuilder.build();
+  }
+
+  /**
+   * Display info (using telemetry) for a recognized AprilTag.
+   */
+  private void telemetryAprilTag() {
+    List<AprilTagDetection> myAprilTagDetections;
+    AprilTagDetection myAprilTagDetection;
+
+    // Get a list of AprilTag detections.
+    myAprilTagDetections = myAprilTagProcessor.getDetections();
+    telemetry.addData("# AprilTags Detected", JavaUtil.listLength(myAprilTagDetections));
+    // Iterate through list and call a function to display info for each recognized AprilTag.
+    for (AprilTagDetection myAprilTagDetection_item : myAprilTagDetections) {
+      myAprilTagDetection = myAprilTagDetection_item;
+      // Display info about the detection.
+      telemetry.addLine("");
+      if (myAprilTagDetection.metadata != null) {
+        telemetry.addLine("==== (ID " + myAprilTagDetection.id + ") " + myAprilTagDetection.metadata.name);
+        // Only use tags that don't have Obelisk in them since Obelisk tags don't have valid location data
+        if (!contains(myAprilTagDetection.metadata.name, "Obelisk")) {
+          telemetry.addLine("XYZ " + JavaUtil.formatNumber(myAprilTagDetection.robotPose.getPosition().x, 6, 1) + " " + JavaUtil.formatNumber(myAprilTagDetection.robotPose.getPosition().y, 6, 1) + " " + JavaUtil.formatNumber(myAprilTagDetection.robotPose.getPosition().z, 6, 1) + "  (inch)");
+          telemetry.addLine("PRY " + JavaUtil.formatNumber(myAprilTagDetection.robotPose.getOrientation().getPitch(), 6, 1) + " " + JavaUtil.formatNumber(myAprilTagDetection.robotPose.getOrientation().getRoll(), 6, 1) + " " + JavaUtil.formatNumber(myAprilTagDetection.robotPose.getOrientation().getYaw(), 6, 1) + "  (deg)");
+        }
+      } else {
+        telemetry.addLine("==== (ID " + myAprilTagDetection.id + ") Unknown");
+        telemetry.addLine("Center " + JavaUtil.formatNumber(myAprilTagDetection.center.x, 6, 0) + "" + JavaUtil.formatNumber(myAprilTagDetection.center.y, 6, 0) + " (pixels)");
+      }
+    }
+    telemetry.addLine("");
+    telemetry.addLine("key:");
+    telemetry.addLine("XYZ = X (Right), Y (Forward), Z (Up) dist.");
+    telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+  }
+
+  /**
+   * returns if the containText is inside of the stringToSearch
+   */
+  private boolean contains(String stringToSearch, String containText) {
+    if (stringToSearch.indexOf(containText) + 1 == 0) {
+      return false;
+    }
+    return true;
+  }
 }
+
 
 
 
